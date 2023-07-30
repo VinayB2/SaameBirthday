da329a3bc90793d2afa32538db1ec2f120ff7b18
VinayB2 <85606385+VinayB2@users.noreply.github.com>
2023-07-30 17:17:27 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const specificDate = new Date("8/1/2003");
+const elapsedTimeParagraph = document.getElementById("elapsed-time");
+setInterval(() => {
+const now = new Date();
+const oneMinute = 60 * 1000;
+const oneHour = 60 * oneMinute;
+const elapsedTime = now - specificDate;
+const oneDay = 24 * oneHour;
+const oneMonth = oneDay * 30.44;
+const oneYear = oneDay * 365.25;
+const years = Math.floor(elapsedTime / oneYear);
+const months = Math.floor((elapsedTime % oneYear) / oneMonth);
+const days = Math.floor((elapsedTime % oneMonth) / oneDay);
+const hours = Math.floor((elapsedTime % oneDay) / oneHour);
+const minutes = Math.floor((elapsedTime % oneHour) / oneMinute);
+const seconds = Math.floor((elapsedTime % oneMinute) / 1000);
+const elapsedTimeString = `It's been ${years} years, ${months} months, ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds since saame's birth`;
+elapsedTimeParagraph.textContent = elapsedTimeString;
+}, 1000);
